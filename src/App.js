50c6dc50711cdf1d795346ba44de64f0a3f12bf3
alok <alok60541@gmail.com>
2022-10-17 20:27:17 +0530

    events & fn work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import User from './User';
 function App() {
 
   // component inside component use
-  function Apple(){
-    return(
-      <div>Apple component</div>
-    );
+  // function Apple(){
+  //   return(
+  //     <div>Apple component</div>
+  //   );
+  // }
+
+  //check working of event and function in react
+  function checkAlert() {
+    alert("function called");
   }
 
   return (
@@ -16,9 +21,17 @@ function App() {
      <h1>Hello world</h1>
      <User />
      
-     {/* component used method 1*/}
-     {Apple()}
-     <Apple />
+     {/* component used method 1 & 2 */}
+     {/* {Apple()}     */}
+     {/* <Apple /> */}
+
+
+    {/* write fn name inside without () to work on click otherwise run auto after page render */}
+    {/* or can use Arrow function ,()=>{} this don't run auto */}
+    <div>
+      {/* <button onClick={checkAlert}>click here</button> */}
+      <button onClick={()=> checkAlert()}>click here</button>
+    </div>
 
     </div>
   );
