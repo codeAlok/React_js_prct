87efd38d0527786015e3bc8139718cf1b3ee9a8a
alok <alok60541@gmail.com>
2022-10-18 14:22:13 +0530

    state in classComp

--- src/App.js
+++ src/App.js
@@ -1,40 +1,79 @@
 // import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 import User from './User';
+import ClassComp from './ClassComp'; //for state & prop in class_comp
+
+// function App() {
+
+//   // component inside component use
+//   // function Apple(){
+//   //   return(
+//   //     <div>Apple component</div>
+//   //   );
+//   // }
+
+//   //check working of event and function in react
+//   function checkAlert() {
+//     alert("function called");
+//   }
+
+//   return (
+//     <div className="App">
+//      <h1>Hello world</h1>
+//      <User />
+     
+//      {/* component used method 1 & 2 */}
+//      {/* {Apple()}     */}
+//      {/* <Apple /> */}
+
+
+//       {/* write fn name inside without () to work on click otherwise run auto after page render */}
+//       {/* or can use Arrow function ,()=>{} this don't run auto */}
+//       <div>
+//         {/* <button onClick={checkAlert}>click here</button> */}
+//         <button onClick={()=> checkAlert()}>click here</button>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+
+// *** state in react in fn_component ***
+// state is to store variable in react
+// through normal variable we cannot update the data. 
 
 function App() {
-
-  // component inside component use
-  // function Apple(){
-  //   return(
-  //     <div>Apple component</div>
-  //   );
+  // let data = "alok";
+  // function updateData() {
+  //   data = "aditya";
+  //   alert(data);
   // }
 
-  //check working of event and function in react
-  function checkAlert() {
-    alert("function called");
-  }
+  // // *** Using state ***
+  // const [data, setData] =  useState(0); //default value
 
-  return (
-    <div className="App">
-     <h1>Hello world</h1>
-     <User />
-     
-     {/* component used method 1 & 2 */}
-     {/* {Apple()}     */}
-     {/* <Apple /> */}
+  // function updateData() {
+  //   setData(data+1); // to update according to previous data
+  // }
 
+  // console.warn("___Rendered__");
 
-      {/* write fn name inside without () to work on click otherwise run auto after page render */}
-      {/* or can use Arrow function ,()=>{} this don't run auto */}
-      <div>
-        {/* <button onClick={checkAlert}>click here</button> */}
-        <button onClick={()=> checkAlert()}>click here</button>
-      </div>
+  // return (
+  //   <div>
+  //     {/* updated data will not show here if not use state property*/}
+  //     <h1>{data}</h1> 
+  //     <button onClick={updateData}>Update Now</button>
+  //   </div>
+  // );
 
+  //*** state in class_component***
+  return(
+    <div>
+      <ClassComp />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/ClassComp.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+
+class ClassComp extends Component {
+    constructor() {
+        super();  // must use super()
+        
+        this.state = {
+            data: 1
+        }
+    }
+
+    apple() {
+        this.setState({data: this.state.data + 1});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.data}</h1>
+                <button onClick={()=> this.apple()}>click</button>
+            </div>
+        )
+    }
+}
+
+export default ClassComp;
